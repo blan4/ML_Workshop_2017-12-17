d60cfb2413902efa22425f723692619a40f573c0
Ilya Siganov <ilya.blan4@gmail.com>
2017-12-12 22:21:18 +0600

    add digits drawer

--- Android/app/src/main/java/org/senior_sigan/digits/MainActivity.kt
+++ Android/app/src/main/java/org/senior_sigan/digits/MainActivity.kt
@@ -1,12 +1,48 @@
 package org.senior_sigan.digits
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+import org.jetbrains.anko.find
+import org.senior_sigan.digits.views.DrawModel
+import org.senior_sigan.digits.views.DrawView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawView: DrawView
+    private val PIXEL_WIDTH = 28
+    private val drawModel = DrawModel(PIXEL_WIDTH, PIXEL_WIDTH)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawView = find(R.id.draw)
+        drawView.setModel(drawModel)
+        drawView.setOnTouchListener(TouchListener(drawView, drawModel))
+
+        val resText: TextView = find(R.id.tfRes)
+
+        find<Button>(R.id.btn_clear).setOnClickListener {
+            drawModel.clear()
+            drawView.reset()
+            drawView.invalidate()
+        }
+
+        find<Button>(R.id.btn_class).setOnClickListener {
+            val pixels = drawView.pixelData
+            resText.text = "???"
+        }
+    }
+
+    override fun onResume() {
+        drawView.onResume()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        drawView.onPause()
+        super.onPause()
     }
 }
---
+++ Android/app/src/main/java/org/senior_sigan/digits/TouchListener.kt
@@ -0,0 +1,62 @@
+package org.senior_sigan.digits
+
+import android.graphics.PointF
+import android.view.MotionEvent
+import android.view.View
+import org.senior_sigan.digits.views.DrawModel
+import org.senior_sigan.digits.views.DrawView
+
+class TouchListener(
+        private val drawView: DrawView,
+        private val drawModel: DrawModel
+) : View.OnTouchListener {
+    private var mTmpPiont = PointF()
+    private var mLastX: Float = 0f
+    private var mLastY: Float = 0f
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        val action = event.action and MotionEvent.ACTION_MASK
+        return when (action) {
+            MotionEvent.ACTION_DOWN -> {
+                processTouchDown(event)
+                true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                processTouchMove(event)
+                true
+            }
+            MotionEvent.ACTION_UP -> {
+                processTouchUp()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun processTouchDown(event: MotionEvent) {
+        mLastX = event.x
+        mLastY = event.y
+        drawView.calcPos(mLastX, mLastY, mTmpPiont)
+        val lastConvX = mTmpPiont.x
+        val lastConvY = mTmpPiont.y
+        drawModel.startLine(lastConvX, lastConvY)
+    }
+
+    private fun processTouchMove(event: MotionEvent) {
+        val x = event.x
+        val y = event.y
+
+        drawView.calcPos(x, y, mTmpPiont)
+        val newConvX = mTmpPiont.x
+        val newConvY = mTmpPiont.y
+        drawModel.addLineElem(newConvX, newConvY)
+
+        mLastX = x
+        mLastY = y
+        drawView.invalidate()
+    }
+
+    private fun processTouchUp() {
+        drawModel.endLine()
+    }
+}
